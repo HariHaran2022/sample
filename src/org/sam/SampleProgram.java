9eed080031c9b095b77b9ba31c3059c5361402a2
HariHaran2022 <hariharan4996@gmail.com>
2022-02-15 20:22:08 +0530

    Msg

---
+++ src/org/sam/SampleProgram.java
@@ -0,0 +1,18 @@
+package org.sam;
+
+public class SampleProgram {
+	
+	public void abc() {
+	System.out.println("First three letters");
+	}
+	public void xyz() {
+		System.out.println("Last three letters");
+	}
+
+	public static void main(String[] args) {
+		SampleProgram sampleProgram = new SampleProgram();
+	sampleProgram.abc();
+	sampleProgram.xyz();
+	}
+
+}
