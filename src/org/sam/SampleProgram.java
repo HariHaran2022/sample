671971b53f6e7b5c90db4e4b19404e27ceae51aa
HariHaran2022 <hariharan4996@gmail.com>
2022-02-15 21:01:56 +0530

    hello

--- src/org/sam/SampleProgram.java
+++ src/org/sam/SampleProgram.java
@@ -8,11 +8,14 @@ public class SampleProgram {
 	public void xyz() {
 		System.out.println("Last three letters");
 	}
-
+    public void d() {
+		System.out.println("Fourth letter");
+	}
 	public static void main(String[] args) {
 		SampleProgram sampleProgram = new SampleProgram();
 	sampleProgram.abc();
 	sampleProgram.xyz();
+	sampleProgram.d();
 	}
 
 }
